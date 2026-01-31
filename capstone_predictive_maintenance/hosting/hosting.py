f664cda246385cd08342c30724f03883cb72ac2c
sourav1511 <nitt.sourav@gmail.com>
2026-01-31 13:59:23 +0000

    latest_Commit_app_fix_data_fix

--- capstone_predictive_maintenance/hosting/hosting.py
+++ capstone_predictive_maintenance/hosting/hosting.py
@@ -4,7 +4,7 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="capstone_predictive_maintenance/deployment",     # the local folder containing your files
-    repo_id="sp1505/Predictive-Maintenance"    # the target repo
+    repo_id="sp1505/Predictive-Maintenance",    # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
